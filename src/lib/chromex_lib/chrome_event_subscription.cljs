d831c1a4c8c99db262847ebe60686c99a9718f05
Antonin Hildebrand <antonin@hildebrand.cz>
2015-11-22 23:35:10 +0100

    use dynamic exception handlers in chrome_event_subscription.cljs

--- src/lib/chromex_lib/chrome_event_subscription.cljs
+++ src/lib/chromex_lib/chrome_event_subscription.cljs
@@ -1,23 +1,43 @@
 (ns chromex-lib.chrome-event-subscription
   (:require [chromex-lib.protocols :as protocols :refer [IChromeEventSubscription IChromeEventChannel]]))
 
+; exception handlers, see https://www.youtube.com/watch?v=zp0OEDcAro0
+(declare *subscribe-called-while-subscribed*)
+(declare *unsubscribe-called-while-not-subscribed*)
+
+; -- ChromeEventSubscription ----------------------------------------------------------------------------------------
+
 (deftype ChromeEventSubscription [chrome-event listener chan ^:mutable subscribed-count]
 
   IChromeEventSubscription
   (subscribe [this]
-    (if (satisfies? IChromeEventChannel chan)
-      (protocols/register chan this))
     (if-not (= subscribed-count 0)
-      (throw "ChromeEventSubscription: subscribe called while subscribed"))
-    (set! subscribed-count (inc subscribed-count))
-    (.addListener chrome-event listener))
+      (*subscribe-called-while-subscribed* this)
+      (do
+        (if (satisfies? IChromeEventChannel chan)
+          (protocols/register chan this))
+        (set! subscribed-count (inc subscribed-count))
+        (.addListener chrome-event listener))))
   (unsubscribe [this]
     (if-not (= subscribed-count 1)
-      (throw "ChromeEventSubscription: unsubscribe called while not subscribed"))
-    (set! subscribed-count (dec subscribed-count))
-    (.removeListener chrome-event listener)
-    (if (satisfies? IChromeEventChannel chan)
-      (protocols/unregister chan this))))
+      (*unsubscribe-called-while-not-subscribed* this)
+      (do
+        (set! subscribed-count (dec subscribed-count))
+        (.removeListener chrome-event listener)
+        (if (satisfies? IChromeEventChannel chan)
+          (protocols/unregister chan this))))))
+
+; -- constructor ----------------------------------------------------------------------------------------------------
 
 (defn make-chrome-event-subscription [chrome-event listener chan]
   (ChromeEventSubscription. chrome-event listener chan 0))
+
+; -- default exception handlers -------------------------------------------------------------------------------------
+
+(defn ^:dynamic *subscribe-called-while-subscribed* [_chrome-event-subscription]
+  (assert false "ChromeEventSubscription: subscribe called while already subscribed")
+  nil)
+
+(defn ^:dynamic *unsubscribe-called-while-not-subscribed* [_chrome-event-subscription]
+  (assert false "ChromeEventSubscription: unsubscribe called while not subscribed")
+  nil)
