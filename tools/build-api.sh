98611f10477a9ffa2cdee52a1c08119a0b9cd320
Antonin Hildebrand <antonin@hildebrand.cz>
2015-11-29 13:54:15 +0100

    generate apps APIs as well

--- tools/build-api.sh
+++ tools/build-api.sh
@@ -21,11 +21,13 @@ ROOT=`pwd`
 SRC="$ROOT/src"
 TOOLS="$ROOT/tools"
 WORKDIR="$TOOLS/_workdir"
+ROOT_README="$ROOT/readme.md"
 SRC_EXTS="$SRC/exts"
 SRC_EXTS_PRIVATE="$SRC/exts_private"
 SRC_EXTS_INTERNAL="$SRC/exts_internal"
-ROOT_README="$ROOT/readme.md"
-#SRC_APPS="$SRC/apps"
+SRC_APPS="$SRC/apps"
+SRC_APPS_PRIVATE="$SRC/apps_private"
+SRC_APPS_INTERNAL="$SRC/apps_internal"
 
 SERVER2_DIR="${CHROMIUM_SRC}chrome/common/extensions/docs/server2"
 APIS_CACHE_FILE="$WORKDIR/apis.cache"
@@ -67,18 +69,25 @@ SHA=`cat "$APIS_LAST_FILE"`
 EXTS_RESULT=`lein run -- --input="$APIS_FILTERED_JSON_FILE" --outdir="$API_SOURCE_DIR/exts" --chromium-sha="$SHA" --filter="exts" | grep "RESULT:"`
 EXTS_PRIVATE_RESULT=`lein run -- --input="$APIS_FILTERED_JSON_FILE" --outdir="$API_SOURCE_DIR/exts_private" --chromium-sha="$SHA" --filter="exts-private" | grep "RESULT:"`
 EXTS_INTERNAL_RESULT=`lein run -- --input="$APIS_FILTERED_JSON_FILE" --outdir="$API_SOURCE_DIR/exts_internal" --chromium-sha="$SHA" --filter="exts-internal" | grep "RESULT:"`
-#lein run -- --input="$APIS_FILTERED_JSON_FILE" --outdir="$API_SOURCE_DIR/apps" --chromium-sha="$SHA" --filter="apps"
+APPS_RESULT=`lein run -- --input="$APIS_FILTERED_JSON_FILE" --outdir="$API_SOURCE_DIR/apps" --chromium-sha="$SHA" --filter="apps" | grep "RESULT:"`
+APPS_PRIVATE_RESULT=`lein run -- --input="$APIS_FILTERED_JSON_FILE" --outdir="$API_SOURCE_DIR/apps_private" --chromium-sha="$SHA" --filter="apps-private" | grep "RESULT:"`
+APPS_INTERNAL_RESULT=`lein run -- --input="$APIS_FILTERED_JSON_FILE" --outdir="$API_SOURCE_DIR/apps_internal" --chromium-sha="$SHA" --filter="apps-internal" | grep "RESULT:"`
+
 popd
 
 rm -rf "$SRC_EXTS"
 rm -rf "$SRC_EXTS_PRIVATE"
 rm -rf "$SRC_EXTS_INTERNAL"
-#rm -rf "$SRC_APPS"
+rm -rf "$SRC_APPS"
+rm -rf "$SRC_APPS_PRIVATE"
+rm -rf "$SRC_APPS_INTERNAL"
 
 mkdir -p "$SRC_EXTS"
 mkdir -p "$SRC_EXTS_PRIVATE"
 mkdir -p "$SRC_EXTS_INTERNAL"
-#mkdir -p "$SRC_APPS"
+mkdir -p "$SRC_APPS"
+mkdir -p "$SRC_APPS_PRIVATE"
+mkdir -p "$SRC_APPS_INTERNAL"
 
 cp -R "$API_SOURCE_DIR/"* "$SRC"
 
@@ -87,16 +96,22 @@ popd
 STATS_HEADER1="| API family | namespaces | properties | functions | events |"
 STATS_HEADER2="| --- | --- | --- | --- | --- |"
 
-PUBLIC_APIS="${EXTS_RESULT/RESULT:/| [Public Chrome Extension APIs](src/exts) |}"
-PRIVATE_APIS="${EXTS_PRIVATE_RESULT/RESULT:/| [Private Chrome Extension APIs](src/exts_private) |}"
-INTERNAL_APIS="${EXTS_INTERNAL_RESULT/RESULT:/| [Internal Chrome Extension APIs](src/exts_internal) |}"
+EXTS_PUBLIC_APIS="${EXTS_RESULT/RESULT:/| [Public Chrome Extension APIs](src/exts) |}"
+EXTS_PRIVATE_APIS="${EXTS_PRIVATE_RESULT/RESULT:/| [Private Chrome Extension APIs](src/exts_private) |}"
+EXTS_INTERNAL_APIS="${EXTS_INTERNAL_RESULT/RESULT:/| [Internal Chrome Extension APIs](src/exts_internal) |}"
+APPS_PUBLIC_APIS="${APPS_RESULT/RESULT:/| [Public Chrome App APIs](src/apps) |}"
+APPS_PRIVATE_APIS="${APPS_PRIVATE_RESULT/RESULT:/| [Private Chrome App APIs](src/apps_private) |}"
+APPS_INTERNAL_APIS="${APPS_INTERNAL_RESULT/RESULT:/| [Internal Chrome App APIs](src/apps_internal) |}"
 
 STATS_TABLE="
 $STATS_HEADER1
 $STATS_HEADER2
-$PUBLIC_APIS
-$PRIVATE_APIS
-$INTERNAL_APIS"
+$EXTS_PUBLIC_APIS
+$EXTS_PRIVATE_APIS
+$EXTS_INTERNAL_APIS
+$APPS_PUBLIC_APIS
+$APPS_PRIVATE_APIS
+$APPS_INTERNAL_APIS"
 
 README_WITH_MARKER=`perl -pe 'BEGIN{undef $/;} s/provides following wrappers:\n.*?Note: Chromex generator uses/provides following wrappers:\nDYNAMICMARKER\n\nNote: Chromex generator uses/smg' "$ROOT_README"`
 NEW_README="${README_WITH_MARKER/DYNAMICMARKER/$STATS_TABLE}"
