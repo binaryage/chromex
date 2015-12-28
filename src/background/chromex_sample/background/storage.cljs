150f67e7605cbf59c950a377769e93ed09e6dd81
Antonin Hildebrand <antonin@hildebrand.cz>
2015-12-28 19:38:17 +0100

    add error checking into storage.cljs example

--- src/background/chromex_sample/background/storage.cljs
+++ src/background/chromex_sample/background/storage.cljs
@@ -12,11 +12,17 @@
                             "key3" true
                             "key4" nil})
     (go
-      (let [[items] (<! (get local-storage))]
-        (log "fetch all:" items)))
+      (let [[[items] error] (<! (get local-storage))]
+        (if error
+          (error "fetch all error:" error)
+          (log "fetch all:" items))))
     (go
-      (let [[items] (<! (get local-storage "key1"))]
-        (log "fetch key1:" items)))
+      (let [[[items] error] (<! (get local-storage "key1"))]
+        (if error
+          (error "fetch key1 error:" error)
+          (log "fetch key1:" items))))
     (go
-      (let [[items] (<! (get local-storage #js ["key2" "key3"]))]
-        (log "fetch key2 and key3:" items)))))
+      (let [[[items] error] (<! (get local-storage #js ["key2" "key3"]))]
+        (if error
+          (error "fetch key2 and key3 error:" error)
+          (log "fetch key2 and key3:" items))))))
