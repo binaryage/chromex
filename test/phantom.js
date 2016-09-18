934d2bc1ab55416049b70d30ed5a3a1e75541764
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-18 14:00:14 +0200

    reformat phantom.js and related files

--- test/phantom.js
+++ test/phantom.js
@@ -10,29 +10,29 @@ var page = require('webpage').create();
 var url = system.args[1];
 
 function testCompilerOutput() {
-  var compilerOutput = fs.read('test/.compiler-out.txt');
-  return page.evaluate(function(output){
-    return chromex.test.compiler.warnings(output);
-  }, compilerOutput);
+    var compilerOutput = fs.read('test/.compiler-out.txt');
+    return page.evaluate(function(output) {
+        return chromex.test.compiler.warnings(output);
+    }, compilerOutput);
 }
 
-page.onConsoleMessage = function (message) {
-  if (message == "TESTS DONE") {
-    var failures = page.evaluate(function(){
-        return window["test-failures"];
-    });
+page.onConsoleMessage = function(message) {
+    if (message == "TESTS DONE") {
+        var failures = page.evaluate(function() {
+            return window["test-failures"];
+        });
 
-    failures += testCompilerOutput();
+        failures += testCompilerOutput();
 
-    phantom.exit(failures?100:0);
-  } else {
-    console.log(message);
-  }
+        phantom.exit(failures ? 100 : 0);
+    } else {
+        console.log(message);
+    }
 };
 
 console.log("Loading URL: " + url);
 
-page.open(url, function (status) {
+page.open(url, function(status) {
     if (status != "success") {
         console.log('Failed to open ' + url);
         phantom.exit(1);
