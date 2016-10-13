eb7b64c996d551abb5e48ccc5598fb40903a347f
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-13 14:37:19 +0200

    bump oops to v0.5.0

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha12" :scope "provided"]
                  [org.clojure/clojurescript "1.9.229" :scope "provided"]
                  [org.clojure/core.async "0.2.391"]
-                 [binaryage/oops "0.3.0"]
+                 [binaryage/oops "0.5.0"]
                  [environ "1.1.0"]]
 
   :clean-targets ^{:protect false} ["target"
