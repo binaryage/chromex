a95f9a0d4ff794bb98c59ca0734a7c004c9b594b
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-13 20:43:57 +0200

    move to next snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/chromex "0.5.2"
+(defproject binaryage/chromex "0.5.3-SNAPSHOT"
   :description "A ClojureScript wrapper for writing Chrome Extensions and Apps."
   :url "https://github.com/binaryage/chromex"
   :license {:name         "MIT License"
--- src/lib/chromex/version.clj
+++ src/lib/chromex/version.clj
@@ -1,6 +1,6 @@
 (ns chromex.version)
 
-(def current-version "0.5.2")                                                                                        ; this should match our project.clj
+(def current-version "0.5.3-SNAPSHOT")                                                                                        ; this should match our project.clj
 
 (defmacro get-current-version []
   current-version)
