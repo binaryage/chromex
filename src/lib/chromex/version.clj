66b5cfd2401cd6a2b29ba2bfcd7314de9c12cfd7
Antonin Hildebrand <antonin@hildebrand.cz>
2017-02-26 23:16:36 +0100

    don't rely on (System/getProperty "java.class.path")
    Use cljs.util/*clojurescript-version* for reliable version detection.
    
    thanks @anmonteiro

--- src/lib/chromex/version.clj
+++ src/lib/chromex/version.clj
@@ -1,18 +1,25 @@
 (ns chromex.version
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [cljs.util]))
 
 (def current-version "0.5.7-SNAPSHOT")                                                                                        ; this should match our project.clj
 
 (defmacro get-current-version []
   current-version)
 
+(defn get-full-clojurescript-version []
+  (string/join "." ((juxt :major :minor :qualifier) cljs.util/*clojurescript-version*)))
+
+(defn broken-clojurescript? [version]
+  (= version "1.9.494"))
+
 (defn do-check-env! []
-  (let [class-path (System/getProperty "java.class.path")]
-    (if (some? (string/index-of class-path "clojurescript-1.9.494.jar"))
+  (let [clojurescript-version (get-full-clojurescript-version)]
+    (if (broken-clojurescript? clojurescript-version)
       (binding [*out* *err*]
         (println
           (str "WARNING: Broken ClojureScript version detected on your class-path.\n"
-               "         ClojureScript 1.9.494 suffers from http://dev.clojure.org/jira/browse/CLJS-1954.\n"
+               "         ClojureScript " clojurescript-version " suffers from http://dev.clojure.org/jira/browse/CLJS-1954.\n"
                "         A solution is to downgrade to ClojureScript 1.9.473"))))))
 
 (defmacro check-env! []
