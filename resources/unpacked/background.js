278d729c8403c0a9b263987534c4494410fd2d8e
Antonin Hildebrand <antonin@hildebrand.cz>
2017-06-26 13:31:09 +0200

    reintroduce figwheel.connect requires mistakenly removed in a07c4fd412bf099cbc66c5a736e6f8932bdc71ff
    issue #3

--- resources/unpacked/background.js
+++ resources/unpacked/background.js
@@ -1,2 +1,3 @@
 goog.require("devtools.preload");
+goog.require("figwheel.connect.build_background");
 goog.require("chromex_sample.background");
--- resources/unpacked/popup.js
+++ resources/unpacked/popup.js
@@ -1,2 +1,3 @@
 goog.require("devtools.preload");
+goog.require("figwheel.connect.build_popup");
 goog.require("chromex_sample.popup");
