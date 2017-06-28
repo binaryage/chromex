1fe85aeff21da07e4a8a8673a1602fef9c25554c
Antonin Hildebrand <antonin@hildebrand.cz>
2017-06-28 19:31:30 +0200

    version 0.5.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/chromex "0.5.8"
+(defproject binaryage/chromex "0.5.9"
   :description "A ClojureScript wrapper for writing Chrome Extensions and Apps."
   :url "https://github.com/binaryage/chromex"
   :license {:name         "MIT License"
