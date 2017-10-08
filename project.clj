binaryage/chromex

commit 9ed2c044866f79c3faf7d71a52c778383a7d4208

participate in canary tests
https://github.com/cljs-oss/canary

project.clj
@@ -1,3 +1,4 @@
+(def clojurescript-version (or (System/getenv "CANARY_CLOJURESCRIPT_VERSION") "1.9.671"))
 (defproject binaryage/chromex "0.5.9"
   :description "A ClojureScript wrapper for writing Chrome Extensions and Apps."
   :url "https://github.com/binaryage/chromex"
@@ -8,7 +9,7 @@
   :scm {:name "git" :url "https://github.com/binaryage/chromex"}
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha17" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.660" :scope "provided"]
+                 [org.clojure/clojurescript ~clojurescript-version :scope "provided"]
                  [org.clojure/core.async "0.3.443"]
                  [binaryage/oops "0.5.5"]
                  [environ "1.1.0"]]

.travis.yml
@@ -13,4 +13,6 @@ notifications:
 cache:
   directories:
   - "$HOME/.m2"
+before_install:
+- curl -sSL https://raw.githubusercontent.com/cljs-oss/canary/master/scripts/install-canary.sh | bash
 script: lein test-all

test/chromex/runner.cljs
@@ -10,6 +10,7 @@
             [chromex.test.chrome-port]))
 
 (enable-console-print!)
+(println "ClojureScript version:" *clojurescript-version*)
 
 (defmethod report [::test/default :summary] [m]
   (println "\nRan" (:test m) "tests containing"

commit e1829b7d62768ba6fee178e4ac3ce2e13b5c6b6a

project: remove broken dependencies setup in :lib profile
This caused that releases 0.5.10 and 0.5.11 had empty dependencies.
I broke it in 9ed2c044866f79c3faf7d71a52c778383a7d4208.

project.clj
@@ -41,12 +41,7 @@
 
              :lib           ^{:pom-scope :provided}                                                                           ; ! to overcome default jar/pom behaviour, our :dependencies replacement would be ignored for some reason
                             [:nuke-aliases
-                             {:dependencies   ~(let [project (->> "project.clj"
-                                                               slurp read-string (drop 3) (apply hash-map))
-                                                     test-dep? #(->> % (drop 2) (apply hash-map) :scope (= "test"))
-                                                     non-test-deps (remove test-dep? (:dependencies project))]
-                                                 (with-meta (vec non-test-deps) {:replace true}))                             ; so ugly!
-                              :source-paths   ^:replace ["src/lib"
+                             {:source-paths   ^:replace ["src/lib"
                                                          "src/exts"
                                                          "src/exts_private"
                                                          "src/exts_internal"