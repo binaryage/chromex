c636c52566459a83ba2c96763acdd84c8c8e1511
Antonin Hildebrand <antonin@hildebrand.cz>
2018-03-26 00:21:54 +0200

    make default marshalling functions propagate nils (close #10)

--- src/lib/chromex/marshalling.cljs
+++ src/lib/chromex/marshalling.cljs
@@ -5,15 +5,17 @@
             [chromex.chrome-storage-area :refer [make-chrome-storage-area]]))
 
 (defn from-native-chrome-port [config native-chrome-port]
-  (make-chrome-port config native-chrome-port))
+  (when (some? native-chrome-port)
+    (make-chrome-port config native-chrome-port)))
 
 (defn to-native-chrome-port [_config chrome-port]
-  {:pre [(satisfies? protocols/IChromePort chrome-port)]}
-  (protocols/get-native-port chrome-port))
+  (when (some? chrome-port)
+    (protocols/get-native-port chrome-port)))
 
 (defn from-native-chrome-storage-area [config native-chrome-storage-area]
-  (make-chrome-storage-area config native-chrome-storage-area))
+  (when (some? native-chrome-storage-area)
+    (make-chrome-storage-area config native-chrome-storage-area)))
 
 (defn to-native-chrome-storage-area [_config chrome-storage-area]
-  {:pre [(satisfies? protocols/IChromeStorageArea chrome-storage-area)]}
-  (protocols/get-native-storage-area chrome-storage-area))
+  (when (some? chrome-storage-area)
+    (protocols/get-native-storage-area chrome-storage-area)))
