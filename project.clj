fd3756099e8b812516e745831effecaec9977987
Antonin Hildebrand <antonin@hildebrand.cz>
2018-12-08 21:01:43 +0100

    project: bump deps

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript ~clojurescript-version :scope "provided"]
                  [org.clojure/core.async "0.4.490"]
-                 [binaryage/oops "0.6.3"]
+                 [binaryage/oops "0.6.4"]
                  [environ "1.1.0"]]
 
   :clean-targets ^{:protect false} ["target"
