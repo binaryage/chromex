5f018331561cc6a22797b9c4ca6ec7ed3de720b4
Antonin Hildebrand <antonin@hildebrand.cz>
2019-04-05 19:27:23 +0200

    api-gen: bump project deps

--- tools/api-gen/project.clj
+++ tools/api-gen/project.clj
@@ -4,13 +4,13 @@
   :license {:name         "MIT License"
             :url          "http://opensource.org/licenses/MIT"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.cli "0.3.3"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.cli "0.4.2"]
                  [org.clojure/data.json "0.2.6"]
-                 [camel-snake-kebab "0.3.2"]
+                 [camel-snake-kebab "0.4.0"]
                  [de.ubercode.clostache/clostache "1.4.0"]
-                 [funcool/cuerdas "0.6.0"]
-                 [clj-time "0.8.0"]
-                 [cljfmt "0.3.0"]]
+                 [funcool/cuerdas "2.2.0"]
+                 [clj-time "0.15.1"]
+                 [cljfmt "0.6.4"]]
   :source-paths ["src"]
   :main api-gen.core)
