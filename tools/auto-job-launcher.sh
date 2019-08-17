41123d759244850998af0a3af6decec4c73a1b29
Antonin Hildebrand <antonin@hildebrand.cz>
2019-08-17 22:59:36 +0200

    tools: fix broken path in auto-job-launcher

--- tools/auto-job-launcher.sh
+++ tools/auto-job-launcher.sh
@@ -2,5 +2,13 @@
 
 set -e -o pipefail
 
+# ensure we start in root folder
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+set -x
+
+ROOT="$(pwd)"
+TOOLS="$ROOT/tools"
+
 # run the job
 "$TOOLS/auto-job-wrapper.sh" 2>&1
