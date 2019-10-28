a949e018a212f06030c205668af262d6d3ea01be
Antonin Hildebrand <antonin@hildebrand.cz>
2019-10-28 21:07:10 +0100

    version 0.8.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def clojurescript-version (or (System/getenv "CANARY_CLOJURESCRIPT_VERSION") "1.10.520"))
-(defproject binaryage/chromex "0.8.2"
+(defproject binaryage/chromex "0.8.3"
   :description "A ClojureScript wrapper for writing Chrome Extensions and Apps."
   :url "https://github.com/binaryage/chromex"
   :license {:name         "MIT License"
