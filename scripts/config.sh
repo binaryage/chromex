71528dd5cf17308edfeba655f40f9752ec3d60d0
Antonin Hildebrand <antonin@hildebrand.cz>
2019-10-28 20:50:06 +0100

    scripts: update sample project version as well

--- scripts/config.sh
+++ scripts/config.sh
@@ -15,5 +15,6 @@ cd ..
 ROOT=`pwd`
 PROJECT_FILE="project.clj"
 PROJECT_VERSION_FILE="src/lib/chromex/version.clj"
+EXAMPLES_SAMPLE_PROJECT_FILE="examples/sample/project.clj"
 
 popd
--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -16,6 +16,7 @@ fi
 
 sed -i "" -e "s/defproject binaryage\/chromex \".*\"/defproject binaryage\/chromex \"$VERSION\"/g" "$PROJECT_FILE"
 sed -i "" -e "s/def current-version \".*\"/def current-version \"$VERSION\"/g" "$PROJECT_VERSION_FILE"
+sed -i "" -e "s/binaryage\/chromex \".*\"/binaryage\/chromex \"$VERSION\"/g" "$EXAMPLES_SAMPLE_PROJECT_FILE"
 
 # this is just a sanity check
 ./scripts/check-versions.sh
