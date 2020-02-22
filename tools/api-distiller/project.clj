00583e3f3a9023ba74141db47f1b727a3c71e72f
Antonin Hildebrand <antonin@hildebrand.cz>
2020-02-22 12:17:07 +0100

    api-distiller: bump project deps

--- tools/api-distiller/project.clj
+++ tools/api-distiller/project.clj
@@ -4,8 +4,8 @@
   :license {:name         "MIT License"
             :url          "http://opensource.org/licenses/MIT"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/tools.cli "0.4.2"]
-                 [org.clojure/data.json "0.2.6"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.cli "1.0.194"]
+                 [org.clojure/data.json "1.0.0"]]
   :source-paths ["src"]
   :main api-distiller.core)
