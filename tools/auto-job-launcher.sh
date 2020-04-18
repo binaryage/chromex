5afd7abc5d6ff6e4d9403177aa728464a8222a57
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-18 19:32:38 +0200

    update job launcher

--- tools/auto-job-launcher.sh
+++ tools/auto-job-launcher.sh
@@ -10,5 +10,11 @@ set -x
 ROOT="$(pwd)"
 TOOLS="$ROOT/tools"
 
+if [[ "$(uname -n)" == maxime* ]]; then
+  export PATH=/Users/darwin/bin/depot_tools:/Users/darwin/tasks/py2-venv/bin:/Users/darwin/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+  export PYTHONPATH=/Users/darwin/tasks/py2-venv
+  export CHROMIUM_SRC=/Users/darwin/tasks/chromium/src
+fi
+
 # run the job
 "$TOOLS/auto-job-wrapper.sh" 2>&1
